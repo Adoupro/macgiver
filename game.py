277e627f0e12e90ae2f9239a1a8961cf84b5af7a
Adou YAPO AKO <adou.professionnel@gmail.com>
2022-11-16 19:07:27 +0100

    Create game.py

---
+++ game.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+size = (400, 400)
+pygame.display.set_mode(size)
+
+pygame.display.set_caption('Jeu MacGyver')
+
+state = True
+
+while state:
+    pygame.display.update()

8817237884f8f0f9d4e1201516aa93e410d5ff3b
Adou YAPO AKO <adou.professionnel@gmail.com>
2022-11-23 19:21:16 +0100

    Update background

--- game.py
+++ game.py
@@ -1,14 +1,25 @@
 import pygame
 
+# Initialization
 pygame.init()
 
-size = (400, 400)
-pygame.display.set_mode(size)
-
+size = (400, 160)
+win = pygame.display.set_mode(size)
 pygame.display.set_caption('Jeu MacGyver')
+framework = pygame.image.load('images/structures.png')
 
+win.blit(framework, (0, 0))
+
+
+# Refresh
 state = True
 
 while state:
+    
+    win.blit(framework, (0, 0))
+    
     pygame.display.update()
-
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            state = False
